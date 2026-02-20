2488ccb147e15b3d1be8bda96a9745df1ffced68
Rupesh Pandey <pandeyrupesh00@gmail.com>
2026-02-20 14:38:48 +0000

    server configs for deployment

--- empires_of_the_skies/server/src/server.ts
+++ empires_of_the_skies/server/src/server.ts
@@ -1,13 +1,18 @@
-import { Server } from 'boardgame.io/server'
+import "dotenv/config";
+import { Server } from "boardgame.io/server";
 import { MyGame } from "../../src/Game";
+
+const PORT = Number(process.env.PORT) || 8000;
+
+const allowedOrigins = process.env.FRONTEND_URL
+  ? [process.env.FRONTEND_URL, "http://localhost:5173"]
+  : ["http://localhost:5173"];
 
 const server = Server({
   games: [MyGame],
-  origins: ['http://localhost:5173'],
-})
-
-const PORT = Number(process.env.PORT) || 8000
+  origins: allowedOrigins,
+});
 
 server.run(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
