5d6b8383ff71b3c52f0091dd90f10eee2f874d06
Rupesh Pandey <pandeyrupesh00@gmail.com>
2026-03-13 10:54:16 +0000

    Fix GAP-H4: plundering a Legend now advances plunderer's heresy by 1
    Per v4.2 rules, plundering a Legend must advance the plundering
    player's heresy tracker by 1 space. This was previously missing.

--- packages/game/src/moves/plunderLegends/plunder.ts
+++ packages/game/src/moves/plunderLegends/plunder.ts
@@ -1,6 +1,7 @@
 import { Move } from "boardgame.io";
 import { MyGameState } from "../../types";
 import { findNextPlunder } from "../../helpers/findNext";
+import { increaseHeresyWithinMove } from "../../helpers/stateUtils";
 
 const plunder: Move<MyGameState> = (
   { G, ctx, playerID, events, random },
@@ -15,6 +16,8 @@ const plunder: Move<MyGameState> = (
     const lootNameAsResource = lootName as keyof typeof currentTile.loot.colony;
     currentPlayer.resources[lootNameAsResource] += lootAmount;
   });
+  // v4.2: plundering a Legend advances the plunderer's heresy by 1
+  increaseHeresyWithinMove(G, playerID);
   console.log("about to find the next plunder or move to the next phase");
   findNextPlunder(G, events);
 };
